7523bb120f0ef21064b2c78c3a36a68d9de4edef
Gustav Andreasson <gustav.andreasson@gmail.com>
2017-09-18 07:08:35 +0200

    updated deploy.sh, added translation

--- install/deploy.sh
+++ install/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -avze ssh --exclude install --exclude *~ ../* gustav@192.168.1.220:/var/www/html/mahjong/
+rsync -avze ssh --exclude install --exclude *~ --exclude .git ../ gustav@192.168.1.220:/var/www/html/mahjong/
 
